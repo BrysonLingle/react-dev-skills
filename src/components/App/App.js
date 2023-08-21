63098f3d29eef8573335321dc4f556031df2ce67
Bryson lingle <brysonburkett99@gmail.com>
2023-08-21 14:14:19 -0600

    almost finished with part 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,26 @@
 import './App.css';
-  export default function App() {
+import NewSkillForm from '../NewSkillForm';
+import SkillListItem from '../SkillListItem'
+  
+const skills = [
+  { name: "HTML", level: 5 },
+  { name: "CSS", level: 3},
+  { name: "JavaScript", level: 4},
+  { name: "Python", level: 2},
+];
+
+function App() {
   return (
-    <div className="App">
-      <h1>React Dev Skills </h1>
-      <ul>
-        <li>Skilllistitem</li>
-        <li>Skilllistitem</li>
-        <li>Skilllistitem</li>
-      </ul>
-      <hr></hr>
-      <form>
-      <label>SKill<input></input>
-      <label>Level<input type='number'></input> </label>
-      <button>Add Skill</button>
-      </label>
-      </form>
+    <div className='App'>
+      <header className="App-header">
+        <h1>React Dev Skills</h1>
+      <SkillListItem skills={skills} listName={'Skills'} />
+        
+      </header>
     </div>
-  );
+  )
 }
+export default App;
 
 
 
---
+++ src/components/SkillListItem.js
@@ -0,0 +1,6 @@
+import './SkillListItem.css'
+ function SKillListItem({skill}){
+    return(<li>{skill}</li>)
+
+}
+export default SKillListItem
