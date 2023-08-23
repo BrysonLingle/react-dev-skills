205eab686ba7180ad81a8503f72b58b9de797816
Bryson lingle <brysonburkett99@gmail.com>
2023-08-23 16:33:36 -0600

    Bryson Lingle pt3 complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import NewSkillForm from './NewSkillForm.js';
 import SkillListItem from './SkillListItem.js'
-  
-const skills = [
-  { name: "HTML", level: 5 },
-  { name: "CSS", level: 3},
-  { name: "JavaScript", level: 4},
-  { name: "Python", level: 2},
-];
+import { useState } from 'react'
 
 function App() {
+  const [skills, setSkills] = useState([
+    { name: "HTML", level: 5 },
+    { name: "CSS", level: 3 },
+    { name: "JavaScript", level: 4 },
+    { name: "Python", level: 2 },
+  ]);
+
   return (
     <div className='App'>
       <header className="App-header">
@@ -19,15 +20,10 @@ function App() {
             <SkillListItem key={index} skill={skill} index={index} />
           ))}
         </ul>
-        <NewSkillForm/>
-        
+        <NewSkillForm />
       </header>
     </div>
   )
 }
+
 export default App;
-
-
-
-
-
